d9d7d7b27681159508e3d40373c08cba58dd8072
Abdul Wasay <wasay0010@gmail.com>
2024-12-05 01:33:34 +0500

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -16,7 +16,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://note-hub-production-app-client-cyan.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://note-hub-production-app-client.vercel.app');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
