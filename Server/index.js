3fa0aaf0eaa44ac439917ea46d9e81d601522936
Abdul Wasay <wasay0010@gmail.com>
2024-12-05 01:04:53 +0500

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 const corsOptions = {
-    origin: 'https://note-hub-production-app-client-cyan.vercel.app',
+    origin: 'https://note-hub-production-app-client.vercel.app',
     credentials: true,
 };
 
