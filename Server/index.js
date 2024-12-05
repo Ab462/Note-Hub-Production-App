e53b1b8b895cdc3640c8d1fe6f675c62e42707bf
Abdul Wasay <wasay0010@gmail.com>
2024-12-04 22:51:32 -0800

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -10,23 +10,9 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(cors({
-    origin: ["https://note-hub-production-app-client.vercel.app"],
-    methods:["POST","GET"],
+    origin: "https://note-hub-production-app-client.vercel.app",
     credentials: true,
 }));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://note-hub-production-app-client.vercel.app');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-  
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-  next();
-});
-
 app.use(express.json());
 
 app.use("/api/auth", auth);
