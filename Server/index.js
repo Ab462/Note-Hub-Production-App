89f1d2d215ccb30dab6a97d9972b6eefee9cfe97
Abdul Wasay <wasay0010@gmail.com>
2024-12-04 23:28:45 -0800

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -9,8 +9,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(cors({ origin: 'https://note-hub-production-app-client.vercel.app' }));
 app.use(express.json());
+app.use(cors({ origin: 'https://note-hub-production-app-client.vercel.app' }));
 
 app.use("/api/auth", auth);
 app.use("/api/notes", notes);
